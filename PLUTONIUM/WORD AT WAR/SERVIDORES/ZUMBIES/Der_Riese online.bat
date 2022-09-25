a1fea773e6f7090106654e8e71d703c7aa3eb0c3
HACKS.EXE <88611462+HACKS-EXE@users.noreply.github.com>
2022-09-25 03:28:53 -0300

    Add files via upload

---
+++ PLUTONIUM/WORD AT WAR/SERVIDORES/ZUMBIES/Der_Riese online.bat
@@ -0,0 +1,23 @@
+@echo off
+::Paste the server key from https://platform.plutonium.pw/serverkeys here
+set key=FHJ6X6xpcoNSbh7cWRv0VqxBoIilFRG8
+::Name of the config file the server should use. (default: dedicated_zm.cfg)
+set cfg=Der_Riese.cfg
+::Name of the server shown in the title of the cmd window. This will NOT bet shown ingame.
+set name=ZM 1
+::Port used by the server (default: 4976)
+set port=1977
+::Only change this when you don't want to keep the bat files in the game folder. MOST WON'T NEED TO EDIT THIS!
+set gamepath=%cd%
+
+title PlutoniumT4 ZM - %name% - Server restarter
+echo Visit plutonium.pw / Join the Discord (a6JM2Tv) for NEWS and Updates!
+echo Server "%name%" will load %cfg% and listen on port %port% UDP!
+echo To shut down the server close this window first!
+echo (%date%)  -  (%time%) %name% server start.
+
+cd /D %LOCALAPPDATA%\Plutonium
+:server
+start /wait /abovenormal bin\plutonium-bootstrapper-win32.exe t4sp "%gamepath%" -dedicated +set zombiemode 1 +set key %key% +set fs_game %mod% +exec %cfg% +set net_port %port% +map_rotate
+echo (%date%)  -  (%time%) WARNING: %name% server closed or dropped... server restarts.
+goto server
---
+++ PLUTONIUM/WORD AT WAR/SERVIDORES/ZUMBIES/Nacht_Der_Untoten_ONLINE.bat
@@ -0,0 +1,31 @@
+@echo off
+
+::Paste the server key from https://platform.plutonium.pw/serverkeys here
+set key=w5A22S0CzzxiZHmMZpXwp6rMHZCUKYAL
+
+::Name of the config file the server should use. (default: dedicated_zm.cfg)
+set cfg=Nacht_Der_Untoten.cfg
+
+::Name of the server shown in the title of the cmd window. This will NOT bet shown ingame.
+set name=ZM 1
+
+::Port used by the server (default: 4976)
+set port=1981
+
+::Only change this when you don't want to keep the bat files in the game folder. MOST WON'T NEED TO EDIT THIS!
+set gamepath=%cd%
+
+::Mod name (default "")
+set mod=""
+
+title PlutoniumT4 ZM - %name% - Server restarter
+echo Visit plutonium.pw / Join the Discord (a6JM2Tv) for NEWS and Updates!
+echo Server "%name%" will load %cfg% and listen on port %port% UDP!
+echo To shut down the server close this window first!
+echo (%date%)  -  (%time%) %name% server start.
+
+cd /D %LOCALAPPDATA%\Plutonium
+:server
+start /wait /abovenormal bin\plutonium-bootstrapper-win32.exe t4sp "%gamepath%" -dedicated +set zombiemode 1 +set key %key% +set fs_game %mod% +exec %cfg% +set net_port %port% +map_rotate
+echo (%date%)  -  (%time%) WARNING: %name% server closed or dropped... server restarts.
+goto server
